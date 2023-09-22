552171bccff0986500cb6acd3ac47420ef33a67d
Alexandre Bouchard <bouchard@stat.ubc.ca>
2023-09-22 12:23:32 -0700

    Push script

---
+++ push.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -e
+
+EXPECTED_ARGS=2
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: `basename $0` [container, e.g. default] [tag]"
+  exit 1
+fi
+
+container=$1
+tag=$2
+docker_username=alexandrebouchardcote
+
+function is_git_clean {
+
+  mods=`git status --porcelain | tail -n 1`
+  if [ "${#mods}" -gt "0" ]
+  then
+    return 1 # false
+  else
+    return 0 # true
+  fi
+
+}
+
+# return 1 (true) if the git repo is up to date with remote, 0 (false) otherwise
+function is_git_remote_up_to_date {
+
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse @{u})
+
+  if [ $LOCAL = $REMOTE ]
+  then
+      return 0
+  else
+      return 1
+  fi
+
+}
+
+echo "Checking everything is committed.."
+if ! is_git_clean
+then
+  echo "Make sure everything is committed in the project dir before releasing artifacts"
+  exit 1
+fi
+
+echo "Checking everything is committed.."
+if ! is_git_remote_up_to_date
+then
+  echo "Make sure to git pull before releasing artifacts"
+  exit 1
+fi
+
+# tag the git repo
+git tag -a $container:$tag -m "Automatic tag for $container:$tag"
+
+# perform docker operation
+cd $container
+docker buildx create --use
+docker buildx build --platform linux/amd64,linux/arm64 -t $docker_username/$container:$tag --push .
+cd -
+
+# push the tag to git 
+git push
